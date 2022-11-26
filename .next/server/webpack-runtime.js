dfc11dcbe0456fe08bb097ac627717ee4c9e6654
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-11-26 17:48:55 +0000

    about: section complete

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b174dd171f061723")
+/******/ 		__webpack_require__.h = () => ("5e451225b18a015c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e444d2efaf071eb"; }
+/******/ 		__webpack_require__.h = function() { return "76bd01cebd942954"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

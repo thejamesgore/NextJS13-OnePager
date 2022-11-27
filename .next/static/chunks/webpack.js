175b3ddc46c6ecb8dc1741844c54e8e5fe9fa2e1
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-11-27 07:25:51 +0000

    Footer: section complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3c52480270c89d86"; }
+/******/ 		__webpack_require__.h = function() { return "45069daf453281ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
